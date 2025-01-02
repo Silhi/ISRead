1853860d8e3f5631785f865a604092b5d4808248
SILHI <silhisilhi287@gmail.com>
2025-01-02 07:26:43 +0700

    Memperbarui fungsionalitas admin dashboard

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,13 +7,10 @@ import Foundation
 
 import firebase_auth
 import firebase_core
+import mobile_scanner
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-
-import mobile_scanner
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
 }
